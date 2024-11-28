4ad464ac0d1355eb34a0496919f9411a95555af5
Muhammad <talha18513@gmail.com>
2024-11-28 18:44:13 +0500

    home page done

--- Docs/Javascripts/footer.js
+++ Docs/Javascripts/footer.js
@@ -4,8 +4,8 @@ footer.innerHTML = `
 
     <div class="border-t max-w-7xl p-5 w-full mx-auto mb-5">
         <div class="flex items-center justify-center px-5">
-            <a href="./index.html" class="flex lg:gap-2 gap-1 md:h-24 h-20 items-center justify-center ">
-                <img src="./Docs/Assets/logo.png" class="lg:h-16 h-14" alt="">
+            <a href="/index.html" class="flex lg:gap-2 gap-1 md:h-24 h-20 items-center justify-center ">
+                <img src="/Docs/Assets/logo.png" class="lg:h-16 h-14" alt="">
                 <h1
                     class="font-bold lg:text-3xl text-lg lg:max-w-none max-w-[6rem] w-full leading-tight text-[#19384c]">
                     Smart Compare</h1>
@@ -14,11 +14,11 @@ footer.innerHTML = `
         <div class="grid md:grid-cols-3 w-full mx-auto gap-3 max-w-6xl md:mt-4 mt-6">
             <div class="text-center md:text-start">
                 <ul class="space-y-3">
-                    <li><a href="./Mentions-Légales.html" class="text-blue-500 hover:underline"> Mentions Légales</a>
+                    <li><a href="/Mentions-Légales.html" class="text-blue-500 hover:underline"> Mentions Légales</a>
                     </li>
-                    <li><a href="./Mentions-Légales.html" class="text-blue-500 hover:underline"> Conditions
+                    <li><a href="/Mentions-Légales.html" class="text-blue-500 hover:underline"> Conditions
                             Générales</a></li>
-                    <li><a href="./Contact.html" class="text-blue-500 hover:underline"> Contact</a></li>
+                    <li><a href="/Contact.html" class="text-blue-500 hover:underline"> Contact</a></li>
                 </ul>
             </div>
             <div class="md:col-span-2 text-center md:text-start md:mt-0 mt-4">
