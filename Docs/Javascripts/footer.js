71fd85e68ebf009d04ec63af4094b293a877e08d
Muhammad <talha18513@gmail.com>
2024-11-28 12:20:02 +0500

    home page done

---
+++ Docs/Javascripts/footer.js
@@ -0,0 +1,35 @@
+const footer  = document.getElementById("footer")
+
+footer.innerHTML = `
+
+    <div class="border-t max-w-7xl p-5 w-full mx-auto mb-5">
+        <div class="flex items-center justify-center px-5">
+            <a href="./index.html" class="flex lg:gap-2 gap-1 md:h-24 h-20 items-center justify-center ">
+                <img src="./Docs/Assets/logo.png" class="lg:h-16 h-14" alt="">
+                <h1
+                    class="font-bold lg:text-3xl text-lg lg:max-w-none max-w-[6rem] w-full leading-tight text-[#19384c]">
+                    Smart Compare</h1>
+            </a>
+        </div>
+        <div class="grid md:grid-cols-3 w-full mx-auto gap-3 max-w-6xl md:mt-4 mt-6">
+            <div class="text-center md:text-start">
+                <ul class="space-y-3">
+                    <li><a href="./Mentions-Légales.html" class="text-blue-500 hover:underline"> Mentions Légales</a>
+                    </li>
+                    <li><a href="./Mentions-Légales.html" class="text-blue-500 hover:underline"> Conditions
+                            Générales</a></li>
+                    <li><a href="./Contact.html" class="text-blue-500 hover:underline"> Contact</a></li>
+                </ul>
+            </div>
+            <div class="md:col-span-2 text-center md:text-start md:mt-0 mt-4">
+                <ul class="space-y-3">
+                    <li><a href="mailto:contact@financefinder.fr"
+                            class="text-blue-400 hover:underline">contact@financefinder.fr</a></li>
+                    <li><a class="text-blue-500">© 2024 par FinanceFinder</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+`
